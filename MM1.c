3889b21a34c044a1b9ab2ab70ef2f27533ae102a
AB <avakash261@gmail.com>
2019-04-05 14:25:16 +0530

    Default Profile
    Shrinidhi's matrix code updated

---
+++ MM1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+	long long int i,j,k,A[250][250],B[250][250],C[250][250];
+	for(i=0;i<250;i++)
+	{
+		for(j=0;j<250;j++)
+		{
+			for(k=0;k<250;k++)
+			{
+				C[i][j] += A[i][k]*B[k][j];
+			}
+		}
+	}
+}
