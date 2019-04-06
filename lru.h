6ba2a9484b06c8a3eaf0ee24a750ae61cc03285a
shrinidhi99 <shrinidhi99.varna@gmail.com>
2019-04-06 20:19:18 +0530

    made some changes in lru.h

--- lru.h
+++ lru.h
@@ -1,48 +1,47 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
-long long int tagValue[1000000];
-long long int setValue[1000000];
-int lru(long long int tagValue[], long long int setValue[], long long int size, int numberOfWays)
+long long int tagValue;
+long long int setValue;
+long long int value[16384];
+int valid[16384], tim[16384];
+static int count=0;
+int lru(long long int tagValue, long long int setValue, long long int size, int numberOfWays, long long int block_size)
 {
-	double hit=0, miss=0;
-	int i,j,k,temp1,temp2,flag;
+	long long int cache_size = 1024*1024, no_of_blocks = cache_size/block_size;
+	long long int i,j,k,temp1,temp2,flag;
+	long long int no_of_sets = no_of_blocks/numberOfWays;
 	// number of sets, number of ways and set size are variable
 	// block size is 64B and cache size is 1MB
-	long long int cache_size = 1024*1024, no_of_blocks = cache_size/64, block_size = 64;
-	long long int no_of_sets = no_of_blocks/numberOfWays;
-	long long int value[16384];
-	int valid[16384], tim[16384];
 	// LRU algorithm
+	// initialize this only once
 	for(i=0;i<no_of_blocks;i++)
 	{
 		value[i] = 0;
 		valid[i] = 0;
 		tim[i] = 0;
-	}
+	} // create only once
 	
-	for(i=0;i<size;i++)
-	{
-		temp1 = setValue[i]*numberOfWays;
+	
+		temp1 = setValue*numberOfWays;
 		temp2 = temp1+numberOfWays-1;
 		flag = 0;		
 		// setno. ranges from setvalue*numberofways to setvalue*numberofways + numberofways - 1
 		for(j=temp1;j<=temp2;j++)
 		{
-			if(valid[j] == 1 && tagValue[i] == value[j])
+			if(valid[j] == 1 && tagValue == value[j])
 			{
 				// its a HIT
-				++hit;
+				//++hit;
 				//printf("HIT\n");
-				flag = 1;
 				valid[j] = 1;
 				tim[j] = i;
-				break;
+				return 1;
 			}
 		}
-		if(flag!=1) // it is a miss
-		{
-			++miss;
+		// it is a miss
+		
+			//++miss;
 			//printf("MISS\n");
 			flag = 0;
 			for(j=temp1;j<=temp2;j++)
@@ -50,11 +49,11 @@ int lru(long long int tagValue[], long long int setValue[], long long int size, 
 				if(valid[j] == 0)
 				{
 					// allot this empty block to that instruction
-					value[j] = tagValue[i];
+					value[j] = tagValue;
 					flag = 2;
 					tim[j] = i;
 					valid[j] = 1;
-					break;
+					return 0;
 				}
 			}
 			if(flag != 2)
@@ -67,22 +66,14 @@ int lru(long long int tagValue[], long long int setValue[], long long int size, 
 						k = j;
 				}
 				// now we need to replace this block with index stored in 'k'
-				value[k] = tagValue[i];
+				value[k] = tagValue;
 				tim[k] = i;
 				valid[k] = 1;
 				// block replaced and miss has also been incremented
+				return 0;
 			}
-		}
-	}
-	return (hit)/(hit+miss); // returning hit ratio
-}
-int main()
-{
-	long long int size;
-	int numberOfWays;
-	long long int i;
+			
+		
 	
-	lru(tagValue, setValue, size, numberOfWays);
-    return 0;
-    
+	//return (hit)/(hit+miss); // returning hit ratio
 }
