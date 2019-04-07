00ad2059a39823ae406563d82987582ae7548af9
shrinidhi99 <shrinidhi99.varna@gmail.com>
2019-04-07 11:07:07 +0530

    added static int count and a condition to check that

--- lru.h
+++ lru.h
@@ -11,69 +11,58 @@ int lru(long long int tagValue, long long int setValue, long long int size, int 
 	long long int cache_size = 1024*1024, no_of_blocks = cache_size/block_size;
 	long long int i,j,k,temp1,temp2,flag;
 	long long int no_of_sets = no_of_blocks/numberOfWays;
-	// number of sets, number of ways and set size are variable
-	// block size is 64B and cache size is 1MB
+	if(count == 0)
+	{	
+		// initialize this only once
+		for(i=0;i<no_of_blocks;i++)
+		{
+			value[i] = 0;
+			valid[i] = 0;
+			tim[i] = 0;
+		} // create only once
+		count = 1;
+	}	
 	// LRU algorithm
-	// initialize this only once
-	for(i=0;i<no_of_blocks;i++)
+	temp1 = setValue*numberOfWays;
+	temp2 = temp1+numberOfWays-1;
+	flag = 0;		
+	// setno. ranges from setvalue*numberofways to setvalue*numberofways + numberofways - 1
+	for(j=temp1;j<=temp2;j++)
 	{
-		value[i] = 0;
-		valid[i] = 0;
-		tim[i] = 0;
-	} // create only once
-	
-	
-		temp1 = setValue*numberOfWays;
-		temp2 = temp1+numberOfWays-1;
-		flag = 0;		
-		// setno. ranges from setvalue*numberofways to setvalue*numberofways + numberofways - 1
+		if(valid[j] == 1 && tagValue == value[j])
+		{
+			valid[j] = 1;
+			tim[j] = i;
+			return 1;                // return HIT
+		}
+	}
+	flag = 0;
+	for(j=temp1;j<=temp2;j++)
+	{
+		if(valid[j] == 0)
+		{
+			// allot this empty block to that instruction
+			value[j] = tagValue;
+			flag = 2;
+			tim[j] = i;
+			valid[j] = 1;
+			return 0;               // return cold MISS
+		}
+	}
+	if(flag != 2)
+	{
+		k = temp1;
+		// replacement takes place as per the policy
 		for(j=temp1;j<=temp2;j++)
 		{
-			if(valid[j] == 1 && tagValue == value[j])
-			{
-				// its a HIT
-				//++hit;
-				//printf("HIT\n");
-				valid[j] = 1;
-				tim[j] = i;
-				return 1;
-			}
+			if(tim[k]>tim[j])
+				k = j;
 		}
-		// it is a miss
-		
-			//++miss;
-			//printf("MISS\n");
-			flag = 0;
-			for(j=temp1;j<=temp2;j++)
-			{
-				if(valid[j] == 0)
-				{
-					// allot this empty block to that instruction
-					value[j] = tagValue;
-					flag = 2;
-					tim[j] = i;
-					valid[j] = 1;
-					return 0;
-				}
-			}
-			if(flag != 2)
-			{
-				k = temp1;
-				// replacement takes place as per the policy
-				for(j=temp1;j<=temp2;j++)
-				{
-					if(tim[k]>tim[j])
-						k = j;
-				}
-				// now we need to replace this block with index stored in 'k'
-				value[k] = tagValue;
-				tim[k] = i;
-				valid[k] = 1;
-				// block replaced and miss has also been incremented
-				return 0;
-			}
-			
-		
-	
-	//return (hit)/(hit+miss); // returning hit ratio
+		// now we need to replace this block with index stored in 'k'
+		value[k] = tagValue;
+		tim[k] = i;
+		valid[k] = 1;
+		// block replaced and miss has also been incremented
+		return 0;                      // return MISS
+	}
 }
