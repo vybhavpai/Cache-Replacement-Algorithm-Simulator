0afd55ec3f100168278df0af8767e028a9b95bd7
chaitany10 <chaitany.pandiya@gmail.com>
2019-04-07 18:11:41 +0530

    Added final code for testing

--- PLRU.h
+++ PLRU.h
@@ -33,7 +33,12 @@ int node2=0,node4=0,node8=0,node16=0;
 long long int cache_size = 1024*1024;
 int no_of_blocks=0;
 int vreplace = 0;
-int currline=0;
+int currLine=0;
+int i=0;
+int counter;
+int found2=-1,found4=-1,found8=-1,found16=-1;int first=0;
+
+
 void init(int block_size,int n)
 {
     no_of_blocks = cache_size/block_size;
@@ -78,14 +83,12 @@ void init(int block_size,int n)
             for(j=0;j<16;j++)
             {
                 cache16[i].c[j].valid=0;
-                cache16[i].c[j].tag=-1;
+                cache2[i].c[j].tag=-1;
             }
         }
     }
 }
-int i=0;
-int counter;
-int found2=-1,found4=-1,found8=-1,found16=-1;int first=0;
+
 int plru(long long int tagValue, long long int setValue, int numberOfWays,int block_size)
 {
     if(first==0)
@@ -113,13 +116,13 @@ int plru(long long int tagValue, long long int setValue, int numberOfWays,int bl
         if (found2 == -1) {
 
 			vreplace = 0;
-            currline = 0;
+            currLine = 0;
 			for(i=0; i<2 ;i++)
             {
                 if(cache2[setValue].c[i].valid==0)
                 {
                     vreplace=1;
-                    currline=i;
+                    currLine=i;
                 }
             }
 
@@ -136,7 +139,7 @@ int plru(long long int tagValue, long long int setValue, int numberOfWays,int bl
 
 			}
 
-			cache2[setValue].c[currline].tag=tagValue;
+			cache2[setValue].c[currLine].tag=tagValue;
 
 			// Update Nodes for last access (same as read, move to function):
 			// nodes=00x -> line 0
@@ -176,14 +179,14 @@ int plru(long long int tagValue, long long int setValue, int numberOfWays,int bl
         }
         if (found4 == -1) {
             vreplace = 0;
-            currline = 0;
+            currLine = 0;
 
 			for(i=0;i<4;i++)
             {
                 if(cache4[setValue].c[i].valid==0)
                 {
                     vreplace=1;
-                    currline = i;
+                    currLine = i;
                 }
             }
 
@@ -204,16 +207,16 @@ int plru(long long int tagValue, long long int setValue, int numberOfWays,int bl
 
 			}
 
-			cache4[setValue].c[currline].tag=tagValue;
+			cache4[setValue].c[currLine].tag=tagValue;
 
 
-			if (0 == counter)
+			if (0 == currLine)
                 node4 = (node4 & 1) | 6;
-            if (1 == counter)
+            if (1 == currLine)
                 node4 = (node4 & 1) | 4;
-            if (2 == counter)
+            if (2 == currLine)
                 node4 = (node4 & 2) | 1;
-            if (3 == counter)
+            if (3 == currLine)
                 node4 = (node4 & 2) | 0;
 
             return 0;
@@ -251,16 +254,16 @@ int plru(long long int tagValue, long long int setValue, int numberOfWays,int bl
 				return 1;
             }
         }
-        if (found4 == -1) {
+        if (found8 == -1) {
             vreplace = 0;
-            currline = 0;
+            currLine = 0;
 
 			for(i=0;i<8;i++)
             {
                 if(cache8[setValue].c[i].valid==0)
                 {
                     vreplace=1;
-                    cache8[setValue].c
+                    currLine = i;
                 }
             }
 
@@ -269,36 +272,151 @@ int plru(long long int tagValue, long long int setValue, int numberOfWays,int bl
 				// nodes=01x -> line 1
 				// nodes=1x0 -> line 2
 				// nodes=1x1 -> line 3
-				if (0 == (node4 & 6))
+				if (0 == (node8 & 6))
 					currLine = 0;
-				else if (2 == (node4 & 6))
+				else if (2 == (node8 & 6))
 					currLine = 1;
-				else if (4 == (node4 & 5))
+				else if (4 == (node8 & 5))
 					currLine = 2;
-				else if (5 == (node4 & 5))
+				else if (5 == (node8 & 5))
 					currLine = 3;
 
 
 			}
 
-			cache4[setValue].c[currline].tag=tagValue;
+			cache4[setValue].c[currLine].tag=tagValue;
 
 
-			if (0 == counter)
-                node4 = (node4 & 1) | 6;
-            if (1 == counter)
-                node4 = (node4 & 1) | 4;
-            if (2 == counter)
-                node4 = (node4 & 2) | 1;
-            if (3 == counter)
-                node4 = (node4 & 2) | 0;
-
+			if (0 == currLine)
+                node8 = (node8 & 23) | 104;
+            if (1 == currLine)
+                node8 = (node8 & 23) | 96;
+            if (2 == currLine)
+                node8 = (node8 & 27) | 68;
+            if (3 == currLine)
+                node8 = (node8 & 27) | 64;
+            if (4 == currLine)
+                node8 = (node8 & 45) | 18;
+            if (5 == currLine)
+                node8 = (node8 & 45) | 16;
+            if (6 == currLine)
+                node8 = (node8 & 46) | 1;
+            if (7 == currLine)
+                node8 = (node8 & 46) | 0;
             return 0;
 		}
     }
     if(numberOfWays == 16)
     {
+        found16 = -1;
 
+		for (counter = 0; counter < 16 && found16< 0; counter++)
+        {
+            if(cache16[setValue].c[counter].tag==tagValue && cache16[setValue].c[counter].valid==1)
+            {
+                if (0 == counter)
+					node16 = (node16 & 23) | 104;
+				if (1 == counter)
+					node16 = (node16 & 23) | 96;
+				if (2 == counter)
+					node16 = (node16 & 27) | 616;
+				if (3 == counter)
+					node16 = (node16 & 27) | 64;
+                if (4 == counter)
+                    node16 = (node16 & 45) | 116;
+                if (5 == counter)
+                    node16 = (node16 & 45) | 16;
+                if (6 == counter)
+                    node16 = (node16 & 46) | 1;
+                if (7 == counter)
+                    node16 = (node16 & 46) | 0;
+                if (8 == counter)
+                    node16 = (node16 & 46) | 0;
+                if (9 == counter)
+                    node16 = (node16 & 46) | 0;
+                if (10 == counter)
+                    node16 = (node16 & 46) | 0;
+                if (11 == counter)
+                    node16 = (node16 & 46) | 0;
+                if (12 == counter)
+                    node16 = (node16 & 46) | 0;
+                if (13 == counter)
+                    node16 = (node16 & 46) | 0;
+                if (14 == counter)
+                    node16 = (node16 & 46) | 0;
+                if (15 == counter)
+                    node16 = (node16 & 46) | 0;
+
+
+				found16 = counter;
+				return 1;
+            }
+        }
+        if (found16 == -1) {
+            vreplace = 0;
+            currLine = 0;
+
+			for(i=0;i<16;i++)
+            {
+                if(cache16[setValue].c[i].valid==0)
+                {
+                    vreplace = 1;
+                    currLine = i;
+                }
+            }
+
+			if(vreplace=0) {
+				// nodes=00x -> line 0
+				// nodes=01x -> line 1
+				// nodes=1x0 -> line 2
+				// nodes=1x1 -> line 3
+				if (0 == (node16 & 6))
+					currLine = 0;
+				else if (2 == (node16 & 6))
+					currLine = 1;
+				else if (4 == (node16 & 5))
+					currLine = 2;
+				else if (5 == (node16 & 5))
+					currLine = 3;
+
+
+			}
+
+			cache4[setValue].c[currLine].tag=tagValue;
+
+
+			if (0 == currLine)
+                node16 = (node16 & 23) | 104;
+            if (1 == currLine)
+                node16 = (node16 & 23) | 96;
+            if (2 == currLine)
+                node16 = (node16 & 27) | 616;
+            if (3 == currLine)
+                node16 = (node16 & 27) | 64;
+            if (4 == currLine)
+                node16 = (node16 & 45) | 116;
+            if (5 == currLine)
+                node16 = (node16 & 45) | 16;
+            if (6 == currLine)
+                node16 = (node16 & 46) | 1;
+            if (7 == currLine)
+                node16 = (node16 & 46) | 0;
+            if (8 == currLine)
+                node16 = (node16 & 46) | 0;
+            if (9 == currLine)
+                node16 = (node16 & 46) | 0;
+            if (10== currLine)
+                node16 = (node16 & 46) | 0;
+            if (11 == currLine)
+                node16 = (node16 & 46) | 0;
+            if (12 == currLine)
+                node16 = (node16 & 46) | 0;
+            if (14 == currLine)
+                node16 = (node16 & 46) | 0;
+            if (15 == currLine)
+                node16 = (node16 & 46) | 0;
+            return 0;
+		}
     }
 
 
